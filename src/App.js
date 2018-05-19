4e47399ffa405b158d755c13ba32ac19084747e4
BillyPurvis <purvisbilly@outlook.com>
2018-05-19 18:23:03 +0100

    Adds react-router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,68 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+
+
+const Home = () => (
+  <div className="page-container">
+    <header>
+        <div>
+            <h1>Home</h1>         
+        </div>
+    </header>
+  </div>
+)
+
+const Projects = () => (
+  <div>
+      <h1>Projects</h1>
+  </div>
+)
+
+const Blog = () => (
+  <div>
+      <h1>Blog</h1>
+  </div>
+)
+
+const Contact = () => (
+  <div>
+      <h1>Contact</h1>
+  </div>
+)
+
+const Nav = () => (
+  <Router>
+      <div>
+          <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/projects">Projects</Link></li>
+              <li><Link to="/blog">Blog</Link></li>
+              <li><Link to="/contact">Contact</Link></li>
+          </ul>
+
+          <hr/>
+
+          <Route exact path="/" component={Home}/>
+          <Route path="/projects" component={Projects}/>
+          <Route path="/blog" component={Blog}/>
+          <Route path="/contact" component={Contact}/>
+      </div>
+
+      
+  </Router>
+)
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     <Nav />
     );
   }
 }
