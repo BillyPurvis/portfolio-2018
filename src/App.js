ba1fa1fcbeff1d2ee6c5dbc9cc2b06225e931e94
BillyPurvis <purvisbilly@outlook.com>
2018-05-25 10:41:44 +0100

    Add new home page grid layout

--- src/App.js
+++ src/App.js
@@ -12,10 +12,37 @@ import './App.css';
 import ProjectHeader from './Scenes/Project/Components/project-header'
 import Navigation from './Components/Navigation/navigation'
 
+import Logo from './Components/Header/images/logo.svg'
+import PlacedImage from './Components/Home/images/placed-app.jpg'
+import SugarLogImage from './Components/Home/images/sugar-log-app.jpg'
+import VueAppImage from './Components/Home/images/vue-app.jpg'
+
+
+
+
+const ProjectGrid = () => (
+  <div className="grid-container">
+    <div className="grid-item">
+      <img src={PlacedImage} alt="Placed App Webpage Image"/>
+    </div>
+    <div className="grid-item">
+      <img src={SugarLogImage} alt="SugarLog App Logo"/>
+    </div>
+    <div className="grid-item">
+      <img src={VueAppImage} alt="Vue App Image"/>
+    </div>        
+  </div>
+  
+)
 
 const Home = () => (
   <div>
-    <h1>Home</h1>
+    <div className="page-heading">
+    <h1>Hey, my name is Billy. I'm a Full Stack developer in London
+specialising in PHP,  Laravel and Symfony.</h1>
+  </div>
+  <ProjectGrid />
+  
   </div>
 )
 
@@ -37,14 +64,16 @@ const Contact = () => (
   </div>
 )
 
+
 class App extends Component {
   render() {
     return (
       <div className="page-container">              
         <Router>
           <div className="page-body">
+            <img id="main-logo" src={Logo} alt="Billy Purvis Logo"/>
             <Navigation />
-            <div className="page-container">                                    
+            <div className="page-content">                                    
                 <Route exact path="/" component={Home}/>
                 <Route exact path="/projects" component={Projects}/>
                 <Route exact path="/blog" component={Blog}/>
